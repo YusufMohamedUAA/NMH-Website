15e2c80d8fd00aea232a762c75dc0d1a32e8ec5a
YusufMohamedUAA <116108051+HairyMilkdud@users.noreply.github.com>
2025-10-17 04:22:26 -0700

    new repo

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import {useState, useRef, useEffect} from 'react';
+import React, { useState } from 'react';
 import '../fonts.css';
 import '../App.css';
-import { ChakraProvider, Box, Heading, Text, Image, Button, extendTheme, Menu, MenuButton, MenuItem, MenuList, IconButton } from '@chakra-ui/react';
-import { HamburgerIcon } from '@chakra-ui/icons'; 
-import { Typewriter } from 'react-simple-typewriter';
+import { ChakraProvider, Box, Image, Menu, MenuButton, MenuItem, MenuList, IconButton, extendTheme } from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
+import { Link } from "react-router-dom";
 import Logo from '../NoMoreHeroesTransparent.png';
 import circlegif from '../circle.gif';
-import { Outlet, Link, Route, BrowserRouter as Router } from "react-router-dom";
-import { NavLink } from 'react-router-dom';
 import MapSD from '../Map.png';
 import Motel1 from '../Motel1.png';
 import Naomi1 from '../Naomi\'s Lab1.png';
@@ -39,58 +36,28 @@ const theme = extendTheme({
 
 const Home = () => {
   const [isGifVisible, setIsGifVisible] = useState(true);
-  const [isMenuOpen, setIsMenuOpen] = useState(false); 
-  const [isHovered, setIsHovered] = useState(false);
-  const [isHovered2, setIsHovered2] = useState(false);
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [hoveredMarker, setHoveredMarker] = useState(null);
 
   const handleIconClick = () => {
-    /* if we want the gif to stay after the click and we are hovering over it
+    setIsGifVisible(false);
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleMouseEnter = () => {
     if (isMenuOpen) {
-        setIsGifVisible(false); 
-      } else {
-        setIsGifVisible(true); 
-      }
-      setIsMenuOpen(!isMenuOpen); 
-      */
-
-        setIsGifVisible(false); 
-        setIsMenuOpen(!isMenuOpen);
+      setIsGifVisible(false);
+    } else {
+      setIsGifVisible(true);
+    }
   };
 
-  const handleMouseEnter = () => { //hover case for menu button
-    if (isMenuOpen) {
-        setIsGifVisible(false); 
-      } else {
-        setIsGifVisible(true); 
-      }
-       
-  };
-
-  const handleMarkerEnter1 = () => {
-    setIsHovered(true);
-  };
-
-  const handleMarkerLeave1 = () => {
-    setIsHovered(false);
-  };
-
-  const handleMarkerEnter2 = () => {
-    setIsHovered2(true);
-  };
-
-  const handleMarkerLeave2 = () => {
-    setIsHovered2(false);
-  };
-  console.log("map " + isGifVisible);
   return (
     <ChakraProvider theme={theme}>
-      
-        <nav>
-          /** */
-          <Box position="absolute" top="40px" right="40px" zIndex="10">
-            <Menu isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} onInteractionOutside={() => setIsMenuOpen(false)}>
-                <div className='icon-container'>
+      <nav>
+        <Box position="absolute" top="40px" right="40px" zIndex="10">
+          <Menu isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)}>
+            <div className='icon-container'>
               <MenuButton
                 as={IconButton}
                 icon={<HamburgerIcon />}
@@ -100,92 +67,106 @@ const Home = () => {
                 aria-label="Menu"
                 onMouseEnter={handleMouseEnter}
                 onClick={handleIconClick}
-
               />
-                {isGifVisible && <img className="gif-overlay" src={circlegif} alt="scribble circle AAAAAAAAAAA" />}
-                
-                </div>
-              <MenuList bg="#141414" borderColor="nmh.primary">
+              {isGifVisible && <img className="gif-overlay" src={circlegif} alt="scribble circle" />}
+            </div>
+            <MenuList bg="#141414" borderColor="nmh.primary">
               <Link to="/" style={{ textDecoration: 'none', display: 'block' }}>
-                <MenuItem
-                  color="nmh.primary"
-                  bg="#141414"
-                  _hover={{ bg: 'nmh.primary', color: 'black' }}
-                  fontSize="lg"
-                >
-                  Home
-                </MenuItem>
-                </Link>
-                <Link to="/about" style={{ textDecoration: 'none', display: 'block' }}>
-                  <MenuItem
-                    color="nmh.primary"
-                    bg="#141414"
-                    _hover={{ bg: 'nmh.primary', color: 'black' }}
-                    fontSize="lg"
-                   
-                  >
-                    About
-                  </MenuItem>
-                </Link>
-                <Link to="/contact" style={{ textDecoration: 'none', display: 'block' }}>
-                  <MenuItem
-                    color="nmh.primary"
-                    bg="#141414"
-                    _hover={{ bg: 'nmh.primary', color: 'black' }}
-                    fontSize="lg"
-                   
-                  >
-                    Contact
-                  </MenuItem>
-                </Link>
-              </MenuList>
-            </Menu>
-          </Box>
-        <Box 
-        position="relative" 
-        top="80px" // Adjust for header height
-        // Remaining height after header
-        overflowY="auto" // Make the map scrollable vertically
-        display="flex"
-        justifyContent="center"
-        alignItems="flex-start" // Align map to the top, so it doesn't start from the middle
-        padding="20px" // Add some padding around the map
-        border="4px solid #E51C23" // Border around the map
-      >
-        <Image src={MapSD} alt="Map" width="100%" />
-        <Marker top="50%" left="50%" isGifVisible={isHovered} handleMouseEnter={handleMarkerEnter1} handleMouseLeave={handleMarkerLeave1} Icon={Motel1}/>
+                <MenuItem color="nmh.primary" bg="#141414" _hover={{ bg: 'nmh.primary', color: 'black' }} fontSize="lg">Home</MenuItem>
+              </Link>
+              <Link to="/about" style={{ textDecoration: 'none', display: 'block' }}>
+                <MenuItem color="nmh.primary" bg="#141414" _hover={{ bg: 'nmh.primary', color: 'black' }} fontSize="lg">About</MenuItem>
+              </Link>
+              <Link to="/contact" style={{ textDecoration: 'none', display: 'block' }}>
+                <MenuItem color="nmh.primary" bg="#141414" _hover={{ bg: 'nmh.primary', color: 'black' }} fontSize="lg">Contact</MenuItem>
+              </Link>
+            </MenuList>
+          </Menu>
+        </Box>
 
-        <Marker top="70%" left="50%" isGifVisible={isHovered2} handleMouseEnter={handleMarkerEnter2} handleMouseLeave={handleMarkerLeave2} Icon={Naomi1}/>
-      </Box>
-      
-        
-        </nav>
-      
+        <Box
+          position="relative"
+          top="80px"
+          overflowY="auto"
+          display="flex"
+          justifyContent="center"
+          alignItems="flex-start"
+          padding="20px"
+          border="4px solid #E51C23"
+        >
+          <Image src={MapSD} alt="Map" width="100%" />
+          <Marker
+            top="50%"
+            left="50%"
+            isHovered={hoveredMarker === 'motel'}
+            onHover={() => setHoveredMarker('motel')}
+            onLeave={() => setHoveredMarker(null)}
+            Icon={Motel1}
+          />
+          <Marker
+            top="70%"
+            left="50%"
+            isHovered={hoveredMarker === 'naomi'}
+            onHover={() => setHoveredMarker('naomi')}
+            onLeave={() => setHoveredMarker(null)}
+            Icon={Naomi1}
+          />
+        </Box>
+
+        {/* Overlay effects */}
+        {hoveredMarker && (
+          <>
+            {/* Bottom white strip */}
+            <Box
+              position="fixed"
+              bottom="0"
+              left="0"
+              width="100%"
+              height="50vh"
+              bg="white"
+              zIndex="20"
+              opacity="0.95"
+            />
+            {/* Top-left image */}
+            <Box
+              position="fixed"
+              top="0"
+              left="0"
+              width="200px"
+              height="200px"
+              zIndex="21"
+              padding="10px"
+            >
+              <Image
+                src={hoveredMarker === 'motel' ? Motel1 : Naomi1}
+                alt="Hovered Marker Visual"
+                width="100%"
+                height="100%"
+                objectFit="contain"
+              />
+            </Box>
+          </>
+        )}
+      </nav>
     </ChakraProvider>
   );
 };
 
-const Marker = ({ top, left, isGifVisible, handleMouseEnter, handleMouseLeave, Icon }) => {
-  console.log(isGifVisible);
-  const handleClick = () => {
-    console.log("you clicked me har har");
-  };
-  return(
-    <Box 
-      position="absolute"  // Use absolute positioning relative to the parent container (map)
-      top={top} // Position the marker based on the passed props
+const Marker = ({ top, left, isHovered, onHover, onLeave, Icon }) => {
+  return (
+    <Box
+      position="absolute"
+      top={top}
       left={left}
-      transform="translate(-50%, -50%)"  // Center the marker based on its position
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      onClick={handleClick}
+      transform="translate(-50%, -50%)"
+      onMouseEnter={onHover}
+      onMouseLeave={onLeave}
+      onClick={() => console.log("Marker clicked")}
       border='2px'
       borderColor='tomato'
     >
-      <Image src={Icon} alt="Motel Marker" boxSize="50px" />
-      
-      {/* Circle GIF Overlay */}
-      {isGifVisible && (
+      <Image src={Icon} alt="Marker" boxSize="50px" />
+      {isHovered && (
         <Box
           position="absolute"
           bottom={1}
@@ -193,6 +174,7 @@ const Marker = ({ top, left, isGifVisible, handleMouseEnter, handleMouseLeave, I
           width="100%"
           height="100%"
           zIndex="5"
+          pointerEvents="none"  // This fixes the hover issue
         >
           <Image src={circlegif} alt="Circle GIF" style={{ transform: 'scale(1.7)' }} />
         </Box>
@@ -200,6 +182,5 @@ const Marker = ({ top, left, isGifVisible, handleMouseEnter, handleMouseLeave, I
     </Box>
   );
 };
-
 
 export default Home;
